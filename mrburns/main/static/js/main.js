377fa1172d68960ea7ab032fe33ec9157dc590e0
Steven Garrity <steven@silverorange.com>
2014-03-28 14:27:40 -0300

    Start of styles for sharing question (and include bootstrap js locally)

--- mrburns/main/static/js/main.js
+++ mrburns/main/static/js/main.js
@@ -3,3 +3,9 @@ console.log('I bring you love!');
 $( '.stats-panel-tab' ).click(function() {
     $( '.stats-panel' ).toggleClass( "open" );
 });
+
+$( '#choice' ).modal(choiceOptions);
+
+var choiceOptions = {
+    "show" : "true"
+}
